036c8b53490c0723ca845b8d0216434bad079b53
eitanrich <eitan.richardson@gmail.com>
2018-06-03 22:57:31 +0300

    Minor changes

--- ndb.py
+++ ndb.py
@@ -3,6 +3,7 @@ from sklearn.cluster import KMeans
 from scipy.stats import norm
 from matplotlib import pyplot as plt
 
+
 class NDB:
     def __init__(self, training_data=None, number_of_bins=100, whitening=False, max_dims=None):
         self.number_of_bins = number_of_bins
@@ -26,7 +27,7 @@ class NDB:
         n, d = training_samples.shape
         if self.whitening:
             self.training_mean = np.mean(training_samples, axis=0)
-            self.training_std = np.std(training_samples, axis=0) + self.eps
+            self.training_std = np.std(training_samples, axis=0) + self.ndb_eps
 
         if self.max_dims is None and d > 1000:
             # To ran faster, perform binning on sampled data dimension (i.e. don't use all channels of all pixels)
@@ -49,7 +50,7 @@ class NDB:
         self.bin_proportions = label_counts[bin_order] / np.sum(label_counts)
         self.bin_centers = bin_centers[bin_order, :]
         self.ref_sample_size = n
-        print('Done. Reference bin proportions = ', self.bin_proportions)
+        print('Done.')
 
     def calculate_score(self, query_samples, model_label=None):
         """
@@ -88,17 +89,23 @@ class NDB:
         if not models_to_plot:
             models_to_plot = sorted(list(self.cached_results.keys()))
 
+        # Visualize the standard errors using the train proportions and size and query sample size
         train_se = calc_se(self.bin_proportions, self.ref_sample_size,
-                                        self.bin_proportions, self.cached_results[models_to_plot[0]]['N'])
+                           self.bin_proportions, self.cached_results[models_to_plot[0]]['N'])
         plt.bar(np.arange(0, K)+0.5, height=train_se*2.0, bottom=self.bin_proportions-train_se,
                 width=1.0, label='Train', color='gray')
 
+        ymax = 0.0
         for i, model in enumerate(models_to_plot):
             results = self.cached_results[model]
             label = '%s (%i : %.4f)' % (model, results['NDB'], results['JS'])
-            print(model, i, (i+0.25)*w, w)
-            plt.bar(np.arange(0, K)+(i+1.0)*w, results['Proportions'], width=w, label=label)
+            ymax = max(ymax, np.max(results['Proportions']))
+            if K <= 70:
+                plt.bar(np.arange(0, K)+(i+1.0)*w, results['Proportions'], width=w, label=label)
+            else:
+                plt.plot(np.arange(0, K)+0.5, results['Proportions'], '--*', label=label)
         plt.legend(loc='best')
+        plt.ylim((0.0, min(ymax, np.max(self.bin_proportions)*4.0)))
         plt.grid(True)
         plt.title('Binning Proportions Evaluation Results (NDB : JS)')
         plt.show()
@@ -118,7 +125,6 @@ class NDB:
         label_vals, label_counts = np.unique(labels, return_counts=True)
         probs[label_vals] = label_counts / n
         return probs
-
 
     @staticmethod
     def two_proportions_z_test(p1, n1, p2, n2, significance_level=0.05):
@@ -153,21 +159,21 @@ class NDB:
 
 
 if __name__ == "__main__":
-    d=100
-    k=50
+    dim=100
+    k=100
     n_train = k*100
     n_test = k*10
 
-    training_samples = np.random.uniform(size=[n_train, d])
-    ndb = NDB(training_data=training_samples, number_of_bins=k)
+    train_samples = np.random.uniform(size=[n_train, dim])
+    ndb = NDB(training_data=train_samples, number_of_bins=k, whitening=True)
 
-    test_samples = np.random.uniform(high=1.0, size=[n_test, d])
+    test_samples = np.random.uniform(high=1.0, size=[n_test, dim])
     ndb.calculate_score(test_samples, model_label='Test')
 
-    test_samples = np.random.uniform(high=0.9, size=[n_test, d])
+    test_samples = np.random.uniform(high=0.9, size=[n_test, dim])
     ndb.calculate_score(test_samples, model_label='Good')
 
-    test_samples = np.random.uniform(high=0.75, size=[n_test, d])
+    test_samples = np.random.uniform(high=0.75, size=[n_test, dim])
     ndb.calculate_score(test_samples, model_label='Bad')
 
     ndb.plot_results(models_to_plot=['Test', 'Good', 'Bad'])
